35d0a315967f5a4e925c53d979819768662d4fab
JOSDEAD <josdead@estudiantec.cr>
2018-06-10 20:33:38 -0600

    ngx-adming

---
+++ src/app/@theme/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-footer',
+  styleUrls: ['./footer.component.scss'],
+  template: `
+    <span class="created-by">ERP creado por Jose Miguel y Andrew</span>
+  `,
+})
+export class FooterComponent {
+}
